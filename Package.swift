a2d251cc51140e3e5405789c37568591b8c4b092
Carson Katri <carson.katri@gmail.com>
2019-09-04 12:19:19 -0400

    macOS 10.14

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CSS",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
