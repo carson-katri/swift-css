de4060ae087013b10bc225697d2fc34ffb0869d4
Carson Katri <carson.katri@gmail.com>
2019-09-07 08:32:21 -0400

    opacity

--- Sources/CSS/Declarations/Color.swift
+++ Sources/CSS/Declarations/Color.swift
@@ -9,3 +9,8 @@
 public func color(_ color: Color) -> Declaration {
     Declaration(property: .color, value: color.description)
 }
+
+/// Set the opacity of the element
+public func opacity(_ opacity: Double) -> Declaration {
+    Declaration(property: .opacity, value: "\(opacity)")
+}
